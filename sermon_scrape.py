a6f8233a0e95626a5d21ee70af29327806c07663
Luke Lewis <luke.lewis95@gmail.com>
2024-01-09 20:43:37 -0800

    correct parameterized sql queries; ensure the html parsing works (with errors); change how names are saved in db

--- sermon_scrape.py
+++ sermon_scrape.py
@@ -4,9 +4,13 @@
 import mechanicalsoup
 import sqlite3
 import os.path
+import time
+import argparse
+import re
 
 database_path = 'sermon_data.db'
 sermons_path = 'sermons/'
+time_between_calls = 0.03
 
 def setup_database():
     connection = sqlite3.connect(database_path)
@@ -32,25 +36,23 @@ def add_sermon(title, author, link, text):
     elif not text:
         print("No sermon text given")
         exit()
-    elif title.find(" ") != -1 or title.lower() != title:
-        print(title + " by " + author + " title not formatted for saving")
-        exit()
-    elif author.find(" ") != -1 or author.lower() != author:
-        print(title + " by " + author + " author not formatted for saving")
-        exit()
+    # ensure no bad sql characters will get in
+
     # add author, title, link to db
     connection = sqlite3.connect(database_path)
     cursor = connection.cursor()
-    result = cursor.execute("SELECT link FROM sermons WHERE author like ':author' AND title like ':title'", ({"author": author, "title": title}))
+    result = cursor.execute("SELECT link FROM sermons WHERE author=:author AND title=:title", ({"author": author, "title": title}))
     if len(result.fetchall()) > 0:
         print(title + " by " + author + " already in database")
         exit()
     cursor.execute("INSERT into sermons VALUES(:author, :title, :link)", ({"author": author, "title": title, "link": link}))
     connection.commit()
     # save text in file using author, title, text
-    sermon_path = sermons_path + author + "/" + title + ".txt"
-    if not os.path.exists(sermons_path + author):
-        os.makedirs(sermons_path + author)
+    file_name_title = title.replace(" ","").lower()
+    dir_name_author = author.replace(" ","").lower()
+    sermon_path = sermons_path + dir_name_author + "/" + file_name_title + ".txt"
+    if not os.path.exists(sermons_path + dir_name_author):
+        os.makedirs(sermons_path + dir_name_author)
     if os.path.isfile(sermon_path):
         print(title + " by " + author + " already saved in files")
         exit()
@@ -62,11 +64,13 @@ def get_spurgeon_sermons():
     browser = mechanicalsoup.StatefulBrowser()
     browser.set_user_agent('the_lords_bot')
     url = "https://www.spurgeon.org/resource-library/sermons/?fwp_paged="
+    author = 'spurgeon'
     page_number = 0
-    num_pages = 1
+    num_pages = 313
     links = []
     while page_number < num_pages:
         page_number += 1
+        time.sleep(time_between_calls)
         page = browser.get(url + str(page_number))
         sermon_tags = page.soup.find_all(class_='latest-resources__row__single')
         for tag in sermon_tags:
@@ -74,49 +78,88 @@ def get_spurgeon_sermons():
                 links.append(tag.div.a["href"])
 
     for sermon_link in links:
+        time.sleep(time_between_calls)
         sermon_page = browser.get(sermon_link)
         article = sermon_page.soup.find_all(class_='article__body__content')
         if len(article) != 1:
             print("Number of articles found at " + sermon_link + " is " + str(len(article)))
-            exit()
+            return
         article = article[0]
-        sermon_title = article["h2"].string.replace(' ', '_').lower()
+        sermon_title = article.h2.string
+        article.h2.decompose()
         sermon_text = article.get_text()
+        sermon_text = rm_leading_spaces(sermon_text)
         add_sermon(sermon_title, author, sermon_link, sermon_text)
-        exit() #DELETE
+
+def check_db():
+    connection = sqlite3.connect(database_path)
+    cursor = connection.cursor()
+    result = cursor.execute("SELECT * FROM sermons") 
+    print(result.fetchall())
 
 def test_add_sermon():
     connection = sqlite3.connect(database_path)
     cursor = connection.cursor()
-    #print("Testing bad title")
-    #add_sermon("God is Good", "John Tucker", "sermons.com/jt", "This is a test sermon")
-    #result = cursor.execute("SELECT link FROM sermons WHERE author like '%?%' AND title like '%?%'", [author, title])
-    #record = result.fetchall()
-    #if record is None:
-    #    print("Negative Title test passed")
-    #else:
-    #    print("Record inserted when it shouldn't be: " + record)
-#
-    add_sermon("god_is_good", "john_tucker", "sermons.com/jt", "This is a test sermon")
-    result = cursor.execute("SELECT link FROM sermons WHERE author like ':author' AND title like ':title'", ({"author": "john_tucker", "title": "god_is_good"}))
+    test_author = 'John Tucker'
+    test_name = 'God is Good'
+    test_text = "This is a test sermon"
+    test_link = 'sermons.com/jt'
+    add_sermon(test_name, test_author, test_link, test_text)
+    result = cursor.execute("SELECT link FROM sermons WHERE author=:author AND title=:title", ({"author": test_author, "title": test_name}))
     record = result.fetchall()
-    if len(record) == 0:
-        print("No record in db for the add_sermon test")
+    if len(record) != 1:
+        print("Rows returned in query for the add_sermon test: " + str(len(record)))
+        print(record)
     else:
-        print("Record for the positive add_sermon test: " + record[0])
+        record = record[0][0]
+        print("Record for the positive add_sermon test: " + record)
+        if record != test_link:
+            print("test failed. stored link is " + record)
+            return
+        f = open('sermons/john_tucker/god_is_good.txt', "r")
+        if f and f.read() == test_text:
+            print("Test passed")
+        elif not f:
+            print("No sermon file found")
+        else:
+            print("Test failed with the following written to file:\n" + f.read())
 
 def delete_db_records():
     connection = sqlite3.connect(database_path)
     cursor = connection.cursor()
     cursor.execute("DELETE from sermons")
     connection.commit()
+
+def delete_test_file():
+    test_file_path = 'sermons/john_tucker/god_is_good'
+    if os.path.exists(test_file_path):
+        os.rmdir("sermons/john_tucker")
+
+def rm_leading_spaces(text):
+    search_index = 0
+    while text[search_index].isspace():
+        search_index += 1
+    return text[search_index:]
     
 ####################################################
-def main():
+def main(args):
     """ Main entry point of the app """
     setup_database()
+    # if arg test, tst then delete then quit 
+    if args.test:
+        test_add_sermon()
+        delete_db_records()
+        delete_test_file()
+        exit()
+    if args.other:
+        exit()
     get_spurgeon_sermons()
+    check_db()
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test", action="store_true", default=False)
+    parser.add_argument("--other", action="store_true", default=False)
+    args = parser.parse_args()
+    main(args)
