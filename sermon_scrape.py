238ae7127510cd8b146eb191b68df1a625c58d4e
Luke Lewis <llewis@skytrac.org>
2024-01-27 23:17:48 -0800

    test file added

--- sermon_scrape.py
+++ sermon_scrape.py
@@ -104,66 +104,24 @@ def get_spurgeon_sermons():
         sermon_text = rm_leading_spaces(sermon_text)
         add_sermon(sermon_title, author, sermon_link, sermon_text)
 
-def check_db():
-    connection = sqlite3.connect(database_path)
-    cursor = connection.cursor()
-    result = cursor.execute("SELECT * FROM sermons") 
-    print(result.fetchall())
-
-def test_add_sermon():
-    connection = sqlite3.connect(database_path)
-    cursor = connection.cursor()
-    test_author = 'John Tucker'
-    test_name = 'God is Good'
-    test_text = "This is a test sermon"
-    test_link = 'sermons.com/jt'
-    add_sermon(test_name, test_author, test_link, test_text)
-    result = cursor.execute("SELECT link FROM sermons WHERE author=:author AND title=:title", ({"author": test_author, "title": test_name}))
-    record = result.fetchall()
-    if len(record) != 1:
-        print("Rows returned in query for the add_sermon test: " + str(len(record)))
-        print(record)
-    else:
-        record = record[0][0]
-        print("Record for the positive add_sermon test: " + record)
-        if record != test_link:
-            print("test failed. stored link is " + record)
-            return
-        f = open('sermons/john_tucker/god_is_good.txt', "r")
-        if f and f.read() == test_text:
-            print("Test passed")
-        elif not f:
-            print("No sermon file found")
-        else:
-            print("Test failed with the following written to file:\n" + f.read())
-
-def delete_db_records():
-    connection = sqlite3.connect(database_path)
-    cursor = connection.cursor()
-    cursor.execute("DELETE from sermons")
-    connection.commit()
-
-def delete_test_file():
-    test_file_path = 'sermons/john_tucker/god_is_good'
-    if os.path.exists(test_file_path):
-        os.rmdir("sermons/john_tucker")
-
 def rm_leading_spaces(text):
     search_index = 0
     while text[search_index].isspace():
         search_index += 1
     return text[search_index:]
+
+def check_db():
+    connection = sqlite3.connect(database_path)
+    cursor = connection.cursor()
+    result = cursor.execute("SELECT COUNT(*) FROM sermons")
+    print(result.fetchall())
+    result = cursor.execute("SELECT TOP 10 * FROM sermons")
+    print(result.fetchall())
     
 ####################################################
 def main(args):
     """ Main entry point of the app """
     setup_database()
-    # if arg test, tst then delete then quit 
-    if args.test:
-        test_add_sermon()
-        delete_db_records()
-        delete_test_file()
-        exit()
     if args.other:
         exit()
     get_spurgeon_sermons()
---
+++ test_scraper.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# tests the scraper module
+
+import sqlite3
+import os.path
+import argparse
+import sermon_scrape
+
+def test_add_normal_sermon():
+    connection = sqlite3.connect(sermon_scrape.database_path)
+    cursor = connection.cursor()
+    test_author = 'John Tucker'
+    test_name = 'God is Good'
+    test_text = "This is a test sermon"
+    test_link = 'sermons.com/jt'
+    result_author = 'john-tucker'
+    result_name = 'god-is-good'
+    print("Add Sermon Test")
+    sermon_scrape.add_sermon(test_name, test_author, test_link, test_text)
+
+    result = cursor.execute("SELECT link FROM sermons WHERE author=:author AND title=:title", ({"author": test_author, "title": test_name}))
+    record = result.fetchall()
+    if len(record) != 1:
+        print("Test failed.")
+        print("Rows returned in query: " + str(len(record)))
+        print(record)
+    else:
+        record = record[0][0]
+        # print("Record: " + record)
+        if record != test_link:
+            print("Test failed. Stored link is " + record)
+        else:
+            f = open('sermons/' + result_author + '/' + result_name + '.txt', "r")
+            if f and f.read() == test_text:
+                print("Test passed")
+            elif not f:
+                print("Test failed. No sermon file found")
+            else:
+                print("Test failed with the following written to file:\n" + f.read())
+
+    delete_db_records()
+    delete_test_file(result_author, result_name)
+
+def test_add_weird_sermon_name():
+    connection = sqlite3.connect(sermon_scrape.database_path)
+    cursor = connection.cursor()
+    test_author = 'John T--''&3#$..      uck""er'
+    test_name = 'God is Good2Me'
+    test_text = "This is a test sermon"
+    test_link = 'sermons.com/jt'
+    result_author = 'john-t3-ucker'
+    result_name = 'god-is-good2me'
+    print("Add Weird Sermon Folder Name Test")
+    sermon_scrape.add_sermon(test_name, test_author, test_link, test_text)
+
+    result = cursor.execute("SELECT link FROM sermons WHERE author=:author AND title=:title", ({"author": test_author, "title": test_name}))
+    record = result.fetchall()
+    if len(record) != 1:
+        print("Test failed.")
+        print("Rows returned in query: " + str(len(record)))
+        print(record)
+    else:
+        record = record[0][0]
+        # print("Record: " + record)
+        if record != test_link:
+            print("Test failed. Stored link is " + record)
+        else:
+            f = open('sermons/' + result_author + '/' + result_name + '.txt', "r")
+            if f and f.read() == test_text:
+                print("Test passed")
+            elif not f:
+                print("Test failed. No sermon file found")
+            else:
+                print("Test failed with the following written to file:\n" + f.read())
+
+    delete_db_records()
+    delete_test_file(result_author, result_name)
+
+def delete_db_records():
+    connection = sqlite3.connect(sermon_scrape.database_path)
+    cursor = connection.cursor()
+    cursor.execute("DELETE from sermons")
+    connection.commit()
+
+def delete_test_file(author, title):
+    test_file_path = 'sermons/' + author + '/' + title + '.txt'
+    # use os.walk to delete all contents of the author folder and the author folder itself
+    for root, dirs, files in os.walk('sermons/' + author):
+        for file in files:
+            os.remove(os.path.join(root, file))
+        for dir in dirs:
+            os.rmdir(os.path.join(root, dir))
+    
+####################################################
+def main(args):
+    """ Main entry point of the app """
+    sermon_scrape.setup_database()
+    test_add_normal_sermon()
+    test_add_weird_sermon_name()
+    exit()
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser()
+    # parser.add_argument("--test", action="store_true", default=False)
+    # parser.add_argument("--other", action="store_true", default=False)
+    args = parser.parse_args()
+    main(args)
